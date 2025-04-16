a8086b58827e90ad92afcd75cd3db9b1fed8b58a
Vee-v <m1997vr@gmail.com>
2025-04-16 12:33:02 -0400

    Updated rotational misalignment, saving the measurement.

--- wfsstartup.py
+++ wfsstartup.py
@@ -1,8 +1,10 @@
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 from wfstools import *
 from astropy.visualization import hist
 from scipy.stats import norm
+from pathlib import Path
 
 exp_time = 300
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -25,18 +27,21 @@ def startup(cam):
     thetas = np.array([calculate_rotational_misalignment(frame, cam) for frame in frames])
     theta = np.nanmean(thetas)
     theta_err = np.nanstd(thetas)
+    print(f"Rotational misalignment = {np.rad2deg(theta)} +- {np.rad2deg(theta_err)}")
     plt.figure()
     hist(np.rad2deg(thetas), bins="blocks", histtype='stepfilled', alpha=0.2, density=True, label="Binned data")
     mu, std = norm.fit(np.rad2deg(thetas))
     xmin, xmax = plt.xlim()
     x = np.linspace(xmin, xmax, 100)
     p = norm.pdf(x, mu, std)
+    if not os.path.exists("calib"):
+        os.makedirs("calib")
     plt.plot(x, p, 'k', linewidth=2, label="Fit results: mu = %.5f°,  std = %.5f°" % (mu, std))
     plt.legend()
     plt.xlabel("Angle (°)")
     plt.ylabel("Number of measurements")
+    plt.savefig(Path("calib") / Path("rotational_misalignment.png"))
     plt.show()
-    print(f"Final rotational misalignment = {np.rad2deg(theta)} +- {np.rad2deg(theta_err)}")
     reference_positions = calculate_reference(subap_positions, theta)
     return reference_positions, camera_thread
 
