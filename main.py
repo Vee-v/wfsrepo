8b6187bc6636a5dfe84e528792160be7acdd4887
Vee-v <m1997vr@gmail.com>
2025-05-17 16:13:55 -0400

    Added logic to main wfs loop

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from wfsstartup import startup
 from vmbpy import VmbSystem
 import queue
 import cv2
+import socket
 import numpy as np
+import pickle
 
+host = "10.11.42.189"
+port = 5000
 
 if __name__ == "__main__":
     print("Starting up the camera...")
@@ -20,12 +24,12 @@ if __name__ == "__main__":
             # reduce exposure time
             cam.ExposureTime.set(21.481)
             # Precompute Zernike design matrix for valid subaps
-            input("Remove calibration lamp... \nPress Enter to continue and compute Zernike coefficients...")
-            N_zernike = int(input("How many zernike modes to plot? \n"))
+            input("Remove calibration lamp... \nPress Enter to continue and build imat...")
+            # N_zernike = int(input("How many zernike modes to plot? \n"))
             # Use subaperture center positions for Zernike fit
             subap_positions = calculate_subaperture_positions(grid_size=11, subap_size=28)  # shape [121, 2], in pixels
             valid_subap_positions = subap_positions[valid_subap_mask].detach().cpu()
-            zernike_A = build_zernike_derivative_matrix(valid_subap_positions, N_zernike)
+            # zernike_A = build_zernike_derivative_matrix(valid_subap_positions, N_zernike)
 
             # Use a one-element list for thread-safe frame sharing
             latest_frame = [None]
@@ -33,41 +37,74 @@ if __name__ == "__main__":
             frame_queue_thread.start()
 
             try:
-                while True:
-                    frame = latest_frame[0]
-                    if frame is not None:
-                        if latest_slopes is not None:
-                            zernike_coeffs = fit_slopes_to_zernike(latest_slopes, zernike_A)
-                            # Make the bar plot as tall as the frame, and wide enough for readability
-                            barplot_img = show_zernike_barplot_opencv(zernike_coeffs, height=frame.shape[0], frame_height=frame.shape[0])
-                            # Ensure frame is 3-channel for stacking
-                            if frame.ndim == 2:
-                                frame_bgr = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-                            else:
-                                frame_bgr = frame
-                            # Horizontally stack frame and barplot
-                            combined = np.hstack([frame_bgr, barplot_img])
-                            cv2.imshow('WFS frame', combined)
-                            key = cv2.waitKey(1) & 0xFF
-                            if key == ord('q'):
-                                break
-                        else:
+                with socket.socket() as client_socket:
+                    client_socket.connect((host, port))  # Connect to the DM server
+                    # get number of subaps valid
+                    num_valid_subaps = np.sum(valid_subap_mask.detach().cpu().numpy(), dtype=int).item()
+                    print(f"Number of valid subapertures: {num_valid_subaps}")
+                    # Send the number of slopes to the DM server
+                    client_socket.sendall(num_valid_subaps.to_bytes(4, byteorder='big'))
+                    # interaction matrix building loop
+                    while True:    
+                        msg = client_socket.recv(2086).decode()
+                        if msg != "OK":
+                            print("finished imat")
+                            break
+                        frame = latest_frame[0]
+                        if frame is not None:
+                            latest_slopes = slopes_thread.get_slopes()
+                            if latest_slopes is not None:
+                                # Send slopes to the DM server
+                                slopes = latest_slopes.flatten().detach().cpu().numpy()
+                                pickled = pickle.dumps(slopes.astype(np.float32))
+                                client_socket.send(len(pickled).to_bytes(4, byteorder='big'))
+                                client_socket.send(pickled) 
+                    while True:
+                        frame = latest_frame[0]
+                        if frame is not None:
+                            # if latest_slopes is not None:
+                                # zernike_coeffs = fit_slopes_to_zernike(latest_slopes, zernike_A)
+                                # # Make the bar plot as tall as the frame, and wide enough for readability
+                                # barplot_img = show_zernike_barplot_opencv(zernike_coeffs, height=frame.shape[0], frame_height=frame.shape[0])
+                                # # Ensure frame is 3-channel for stacking
+                                # if frame.ndim == 2:
+                                #     frame_bgr = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+                                # else:
+                                #     frame_bgr = frame
+                                # # Horizontally stack frame and barplot
+                                # combined = np.hstack([frame_bgr, barplot_img])
+                                # cv2.imshow('WFS frame', combined)
+                                # cv2.imshow('WFS frame', frame)
+                                # key = cv2.waitKey(1) & 0xFF
+                                # if key == ord('q'):
+                                #     break
+                            # else:
                             cv2.imshow('WFS frame', frame)
                             key = cv2.waitKey(1) & 0xFF
                             if key == ord('q'):
                                 break
-                    print(f"Frame queue size: {frame_queue.qsize()} ", end=' | ')
-                    cam_fps = camera_thread.get_fps()
-                    slopes_fps = slopes_thread.get_fps()
-                    latest_slopes = slopes_thread.get_slopes()
-                    # Calculate and display Zernike coefficients if slopes are available
-                    if latest_slopes is not None:
-                        zernike_coeffs = fit_slopes_to_zernike(latest_slopes, zernike_A)
-                    print(f"Camera FPS: {cam_fps:.2f} | Slopes FPS: {slopes_fps:.2f}", end='\r')
+                        # print(f"Frame queue size: {frame_queue.qsize()} ", end=' | ')
+                        cam_fps = camera_thread.get_fps()
+                        slopes_fps = slopes_thread.get_fps()
+                        latest_slopes = slopes_thread.get_slopes()
+                        # Calculate and display Zernike coefficients if slopes are available
+                        if latest_slopes is not None:
+                            # send slopes to the DM server
+                            slopes = latest_slopes.flatten().detach().cpu().numpy()
+                            pickled = pickle.dumps(slopes.astype(np.float32))
+                            client_socket.send(len(pickled).to_bytes(4, byteorder='big'))
+                            client_socket.send(pickled) 
+                            msg = client_socket.recv(2086).decode()
+                            if msg != "OK":
+                                print("ERROR: DM server did not respond with OK")
+                                break
+                        #     zernike_coeffs = fit_slopes_to_zernike(latest_slopes, zernike_A)
+                        print(f"Camera FPS: {cam_fps:.2f} | Slopes FPS: {slopes_fps:.2f}", end='\r')
             except KeyboardInterrupt:
-                print("\nStopping all threads and exiting...")
-            finally:
-                frame_queue_thread.stop()
-                camera_thread.stop()
-                slopes_thread.stop()
-                cv2.destroyAllWindows()
+                pass
+            
+            print("\nStopping all threads and exiting...")
+            frame_queue_thread.stop()
+            camera_thread.stop()
+            slopes_thread.stop()
+            cv2.destroyAllWindows()
